040d816ad731c3fba737cbf7796d182ab1412f51
Maybrit Lange <maybritlange@t-online.de>
2025-09-07 20:10:19 +0200

    feat: add users

---
+++ Users.java
@@ -0,0 +1,19 @@
+import java.util.HashMap;
+import java.util.Map;
+
+public class Users {
+    // Vorab definierte Benutzer (username → password)
+    private static final Map<String, String> userMap = new HashMap<>();
+
+    static {
+        userMap.put("kira", "Grinsekatze69");
+        userMap.put("nils", "n1ls");
+        userMap.put("katrin", "katrin123");
+        // Weitere Benutzer hier ergänzen
+    }
+
+    // Prüft Login-Daten
+    public boolean login(String username, String password) {
+        return userMap.containsKey(username) && userMap.get(username).equals(password);
+    }
+}
