b25ccdb5422e2407775f36d43ad512dd44000f80
Maybrit Lange <maybrit.lange@proton.me>
2025-09-10 00:40:18 +0200

    style: rename ChatApp to ChatController

--- Main.java
+++ Main.java
@@ -1,6 +1,6 @@
 public class Main {
     public static void main(String[] args){
-        ChatApp chatApp=new ChatApp();
-        chatApp.run();
+        ChatController chatController=new ChatController();
+        chatController.run();
     }
 }
